fb006db6e2bd57dd8a16d8275b4ee81636b3f01a
chrispy227 <chbppp@gmail.com>
2020-09-24 15:41:36 +0300

    proper quetion and answer key functions enabled

--- py_quiz.py
+++ py_quiz.py
@@ -1,7 +1,6 @@
 from random import seed
 from random import sample
 
-# Create Individual Lists of Question Strings, Answer Choices Strings (a. choice, etc.), ANswer key string, Correct Boolean init val of False, Guesses init INteger val of 3
 # List Layout: {[0]Question:"", [1]Choices:"", [2]Answer:"", [3]Correct:Boolean, [4]Guesses: int(3)}
 Q1 = ["What type of aircraft is a Helicopter?",
       "A.) Fixed Wing\nB.) Rotary Wing\nC.) Hot Air Ballon\nD.) Glider", "b", False, 3]
@@ -15,40 +14,45 @@ QuestionBank = dict({0: Q1, 1: Q2, 2: Q3})
 
 # Random Selector
 def randomQuestion():
+    # Convert QuestionBank Dictionary to a List
     Qlist = list(QuestionBank)
+    # Make RandAskList a global variable
     global RandAskList
-    # This is only for question selection and cannot double index to the question lists
+    # Creates a random ordered list with integers as values to be used to pick questions in another function
     RandAskList = sample(Qlist, len(Qlist))
-
-
-def Quiz():
-    # Make a Random list from Question bank.
-    randomQuestion()
-    # WAys to assign values to multiple variables at once
-    # [Ask1, Ask2, Ask3] = RandAskList[:3]
-    # print(Ask1)
-    # print(Ask2)
-    # print(Ask3)
-    # depths = [[] for i in range(len(RandAskList))] # Creates A List of Empty Lists as long as the mother list. 3 here for now
-
-    # Initialize the Sentry
-    finishedQuestions = 0
-    print(RandAskList[1])
-    # while finishedQuestions < len(RandAskList):
-    #     print(QuestionBank[RandAskList[finishedQuestions]][0])
-    #     print(QuestionBank[RandAskList[finishedQuestions]][1])
-    #     guess = input("Please enter the letter choice for your answer: ")
-    #     if guess == (RandAskList[0][2]):
-    #         pass
-    #     finishedQuestions += 1
-
-
-Quiz()
+    # This is only for question selection and cannot double index to the question lists
 
 
 # Create a Input Validator Function to:
 # --Try to convert to Int, if this passes, then fails validation, Throw a custom error message and ask to try again, do not subtract from guess
 # --If Conversion to Int fails, then make lowercase String and strip leading and trailing edge whitespaces
+def validator():
+    if guess in ["a", "b", "c", "d"]:
+        valdGuess = guess.strip().lower()
+        print(valdGuess)
+
+
+def Quiz():
+    # Make a Random list from Question bank.
+    randomQuestion()
+    # Initialize the Sentry
+    finishedQuestions = 0
+    while finishedQuestions < len(RandAskList):
+        keyIndex = RandAskList[finishedQuestions]
+        correctKey = QuestionBank[keyIndex][2]
+        print(QuestionBank[RandAskList[finishedQuestions]][0])
+        print(QuestionBank[RandAskList[finishedQuestions]][1])
+        global guess
+        guess = input("Please enter the letter choice for your answer: ")
+        while guess != None:
+            validator()
+        if guess == (correctKey):
+            print("YOU WIN MFER!!!!")
+
+        finishedQuestions += 1
+
+
+Quiz()
 
 # Create function to Compare validated String value to Current Question ANSWER KEY
 # -IF passes, Change Correct Boolean to TRUE
